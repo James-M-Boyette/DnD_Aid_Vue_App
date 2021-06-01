b862f132af39c85e836be44cf7dcbaaed71b296f
James Boyette <james.boyette@gmail.com>
2021-06-01 00:00:11 -0400

    Update empty endpoint route to point to 'home' but keep 404 for future use

---
+++ src/views/404.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>Welcome to DnD Aid !</h1>
+    <br />
+    <h1>Sorry - the page you're looking for does not exist !</h1>
+    <h3>
+      (... Or James messed something up. Please let him know so he can fix fix it!)
+      <br />
+      Thank you for your help!
+    </h3>
+    <!-- <img src="@/assets/Imgs/Backgrounds/Black_Market_Entrance_(Hamsterfly).jpg" /> -->
+  </div>
+</template>
+
+<style>
+body {
+  background-image: url("/assets/Imgs/Backgrounds/Black_Market_Entrance_(Hamsterfly).jpg");
+}
+</style>
