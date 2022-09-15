cffeb30c3e1ee287a7a2a42f26ee0279fec4e289
James Boyette <inquiries@james-boyette.com>
2022-09-15 16:42:44 -0400

    Test2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <!-- <h1>{{ message }}</h1> -->
     <!-- <h3>{{ "Brian's my hero" }}</h3> -->
-    <h3>Welcome! Here's my capstone 'DnD aid' app!</h3>
+    <h3>Welcome!!! Here's my capstone 'DnD aid' app!</h3>
     <h5>Please feel free to look around</h5>
     <div></div>
   </div>
